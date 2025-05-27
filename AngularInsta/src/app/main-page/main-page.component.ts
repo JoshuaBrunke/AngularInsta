0d6aa9fe650bc5b0535cc32edaacc19ce8e625c5
Joshua Brunke <seafortghost@gmail.com>
2025-05-27 13:57:53 +0200

    Code formatting

--- AngularInsta/src/app/main-page/main-page.component.ts
+++ AngularInsta/src/app/main-page/main-page.component.ts
@@ -13,41 +13,37 @@ interface Posts {
   standalone: true,
   imports: [RouterModule],
   templateUrl: './main-page.component.html',
-  styleUrl: './main-page.component.scss'
+  styleUrl: './main-page.component.scss',
 })
 export class MainPageComponent {
-posts: Posts[] = [
-  {
-    name: "Hans",
-    image: "../assets/img/banana.jpg",
-    likes: 9,
-    isLiked: false,
-  },
+  posts: Posts[] = [
     {
-    name: "Helene",
-    image: "../assets/img/currant.jpg",
-    likes: 4,
-    isLiked: false,
-  },
+      name: 'Hans',
+      image: '../assets/img/banana.jpg',
+      likes: 9,
+      isLiked: false,
+    },
     {
-    name: "Herbert",
-    image: "../assets/img/orange.jpg",
-    likes: 2,
-    isLiked: false,
-  },
-  
-]
+      name: 'Helene',
+      image: '../assets/img/currant.jpg',
+      likes: 4,
+      isLiked: false,
+    },
+    {
+      name: 'Herbert',
+      image: '../assets/img/orange.jpg',
+      likes: 2,
+      isLiked: false,
+    },
+  ];
 
-likePost(post: Posts) {
-  if (!post.isLiked) {
-    post.likes++;
-    post.isLiked = true;
-  } else {
-    post.likes--;
-    post.isLiked = false;
+  likePost(post: Posts) {
+    if (!post.isLiked) {
+      post.likes++;
+      post.isLiked = true;
+    } else {
+      post.likes--;
+      post.isLiked = false;
+    }
   }
-  
 }
-
-}
-
