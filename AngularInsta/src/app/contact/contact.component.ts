8a6560ab8c6b0d725357a9e89ff3961e6c5a2a62
Joshua Brunke <seafortghost@gmail.com>
2025-05-27 13:41:37 +0200

    Added router links

--- AngularInsta/src/app/contact/contact.component.ts
+++ AngularInsta/src/app/contact/contact.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
+import { Router, RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
@@ -11,3 +11,4 @@ import { Router, RouterModule } from '@angular/router';
 export class ContactComponent {
 
 }
+
