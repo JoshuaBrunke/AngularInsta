d6050e0625c496421569ac3e52abdaeb1973ae4b
Joshua Brunke <seafortghost@gmail.com>
2025-05-28 12:10:21 +0200

    Added single-post component

--- AngularInsta/src/app/app.routes.ts
+++ AngularInsta/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
 import { ContactComponent } from './contact/contact.component';
+import { SinglePostComponent } from './main-page/single-post/single-post.component';
 
 export const routes: Routes = [
     {path: "", component: MainPageComponent},
     {path: "contact", component: ContactComponent},
+    
 ];
